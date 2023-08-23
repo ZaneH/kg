8c30fa336924c956abf26752cf969c172a7bdc79
Zane Helton <zane99@me.com>
2023-08-23 16:48:51 -0400

    Update markdownLinkResolution

--- quartz.config.ts
+++ quartz.config.ts
@@ -57,7 +57,7 @@ const config: QuartzConfig = {
       Plugin.SyntaxHighlighting(),
       Plugin.ObsidianFlavoredMarkdown({ enableInHtmlEmbed: false }),
       Plugin.GitHubFlavoredMarkdown(),
-      Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
+      Plugin.CrawlLinks({ markdownLinkResolution: "absolute" }),
       Plugin.Latex({ renderEngine: "katex" }),
       Plugin.Description(),
     ],
